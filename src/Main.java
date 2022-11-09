8f3eeff59e776f3e67f7381bc28ae9bea0454f31
hutch28 <hutchinsjo@gcc.edu>
2022-11-09 13:12:46 -0500

    add second message

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello my Class");
+        System.out.println("msg 2");
 
         String[] sarr = new String[5];
         ArrayList<String> sArrLst = new ArrayList<>();
